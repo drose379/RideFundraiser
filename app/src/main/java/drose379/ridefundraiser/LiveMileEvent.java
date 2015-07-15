9701efe1e3e92de1dc9ef427b918ad63a399cb9b
Dylan <dylanrose60@gmail.com>
2015-07-15 16:28:10 -0400

    working with LiveMileEvent

--- app/src/main/java/drose379/ridefundraiser/LiveMileEvent.java
+++ app/src/main/java/drose379/ridefundraiser/LiveMileEvent.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.os.Bundle;
 import android.content.Intent;
+import android.widget.TextView;
 
 
 /**
@@ -18,9 +19,16 @@ public class LiveMileEvent extends AppCompatActivity {
 	private LiveMileEventHelper eventHelper;
 	private GPSHelper gpsHelper;
 
+	private TextView distanceMeasure;
+	private TextView timeMeasure;
+
 	@Override
 	public void onCreate(Bundle savedInstance) {
 		super.onCreate(savedInstance);
+
+		distanceMeasure = (TextView) findViewById(R.id.distanceMeasure);
+		timeMeasure = (TextView) findViewById(R.id.timeMeasure);
+
 		eventHelper = getIntent().getBundleExtra("extra").getParcelable("helperInstance");
 		gpsHelper = GPSHelper.getInstance(getApplicationContext());
 	}
