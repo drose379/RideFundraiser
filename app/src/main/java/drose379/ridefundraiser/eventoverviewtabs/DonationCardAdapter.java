4db4e3fccc060d06c46aaedd2e0f9b035596b1ac
Dylan <dylanrose60@gmail.com>
2015-07-30 11:07:17 -0400

    implementing donation cards

---
+++ app/src/main/java/drose379/ridefundraiser/eventoverviewtabs/DonationCardAdapter.java
@@ -0,0 +1,9 @@
+package drose379.ridefundraiser.eventoverviewtabs;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Dylan on 7/30/15.
+ */
+public class DonationCardAdapter extends BaseAdapter {
+}
--- app/src/main/java/drose379/ridefundraiser/eventoverviewtabs/DonationOverviewFragment.java
+++ app/src/main/java/drose379/ridefundraiser/eventoverviewtabs/DonationOverviewFragment.java
@@ -3,10 +3,14 @@ package drose379.ridefundraiser.eventoverviewtabs;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import drose379.ridefundraiser.R;
 
@@ -25,14 +29,28 @@ public class DonationOverviewFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup parent,Bundle savedInstance) {
-        super.onCreateView(inflater,parent,savedInstance);
+        super.onCreateView(inflater, parent, savedInstance);
 
         View v = inflater.inflate(R.layout.donation_overview_frag,parent,false);
         TextView headLabel = (TextView) v.findViewById(R.id.headText);
-        
+
         headLabel.setText("My Donation To - " + eventInfo.getString("organization"));
 
         return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        try {
+            populateDonationCards();
+        } catch (JSONException e) {
+            //No donations submitted from other users, just show one card with original users donations
+        }
+    }
+
+    public void populateDonationCards() throws JSONException {
+        JSONObject donations = new JSONObject(eventInfo.getString("donationSummary"));
     }
 
 }
